c9089c1049764f4021512d133638dac1d04da576
Antonio Villegas <antonio_villegas@hotmail.com>
2023-02-10 01:01:39 -0600

    Deployment test

---
+++ docs/950.e6072203d567e200.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmg_inversiones_main=self.webpackChunkmg_inversiones_main||[]).push([[950],{950:(m,a,s)=>{s.r(a),s.d(a,{PagesModule:()=>n});var e=s(895),o=s(256);class n{}n.\u0275fac=function(l){return new(l||n)},n.\u0275mod=o.oAB({type:n}),n.\u0275inj=o.cJS({imports:[e.ez]})}}]);
